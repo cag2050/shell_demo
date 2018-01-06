b30e52f9b5e3441a67d75fe72d7ab76f7958faa0
cag2050 <cag2050@163.com>
2018-01-06 10:16:15 +0800

    m

---
+++ variable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 定义变量时，不加美元符号
+my_name='cag'
+
+: '
+使用变量，只需要在变量名前面加美元符号
+推荐给所有变量加上花括号，加花括号是为了帮助解释器识别变量的边界，如：${skill}Script
+变量，可以被重新定义
+'
+echo $my_name
+echo ${my_name}
+
+# 只读变量前面加 readonly
+myUrl="http://www.w3cschool.cc"
+readonly myUrl
